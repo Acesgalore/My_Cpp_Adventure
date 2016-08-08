ffa7dbbae6590c6f3bccfd0a4a242ccad9e72254
Acesgalore <acwiller@gmail.com>
2016-08-08 00:08:55 -0700

    Update 7-messin around.cpp

--- 7-messin around.cpp
+++ 7-messin around.cpp
@@ -1,6 +1,7 @@
 // messin around.cpp : Defines the entry point for the console application.
 // Nothing really new here
 // Just went along with the next problem
+// This program asks for a name and greets you if your name is alice or bob
 
 #include "stdafx.h"
 #include <iostream>
